27fd586da2aede87f96a7a6ec282adec50cb4c3c
Akanksha Jadon <akankshajadon@gmail.com>
2020-07-15 10:12:07 +0530

    filter-floating button

--- src/components/dashboard/dashboardHeader.vue
+++ src/components/dashboard/dashboardHeader.vue
@@ -47,7 +47,7 @@
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex';
+// import { mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'dashboardHeader',
--- src/components/dashboard/dashboardSideNav.vue
+++ src/components/dashboard/dashboardSideNav.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <div class="hoarding-nav flex-initial px-4 py-2 fixed top-0 left-0 w-full h-full bg-white transition-all duration-500" style="z-index: 99;" v-bind:class="{hoarding : hoarding}">
+    <div 
+      class="hoarding-nav flex-initial px-4 py-2 fixed top-0 left-0 w-full h-full bg-white transition-all duration-500" 
+      style="z-index: 99;" 
+      v-bind:class="{hoarding : hoarding}">
       <div>
           <h1 class="text-2xl px-4 font-bold text-blue-800 mb-4"> HordingLocation </h1>
           <!-- <div v-for="loc in location" v-bind:key="loc.id" class="text-2xl border border-gray-600 font-bold text-blue-800 hover:bg-jaffa-400"> {{loc.title}} </div> -->
@@ -13,14 +16,16 @@
           </ul>
       </div>
     </div>
-    <button type="button" @click="toggleHoarding" class="fab w-12 h-12 flex rounded-full bg-red-600 text-white text-lg shadow-md fixed p-4" style="right: 1rem; bottom: 2rem; z-index: 999;">
+    <button type="button" @click="toggleHoarding" 
+    class="fab w-12 h-12 flex rounded-full bg-red-600 text-white text-lg shadow-md fixed p-4" 
+    style="right: 1rem; bottom: 2rem; z-index: 999;">
       <i class="fa fa-filter"></i>
     </button>
   </div>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex';
+// import { mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'dashboardSideNav',
